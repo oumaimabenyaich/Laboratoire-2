e3313c9ab0aa71c05a851569124d98461607cf53
Oumaima Benyaich <21237@ecam.be>
2023-03-03 20:46:54 +0100

    enfin réussi

--- Exercice_2.py
+++ Exercice_2.py
@@ -4,3 +4,7 @@
 # Écrivez un programme qui ouvre un fichier texte et qui retrouve tous les nombres qui y apparaissent.
 # Le programme produit en sortie un inventaire de ces nombres, séparés par des virgules et avec le
 # numéro de ligne où ils apparaissent.
+
+file = open ('data.txt', 'r')
+
+print(file.read())
