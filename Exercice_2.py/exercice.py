eee96572143a8f1cc3283242de80472e2429b4e1
Oumaima Benyaich <21237@ecam.be>
2023-03-03 20:29:03 +0100

    commit

--- Exercice_2.py/exercice.py
+++ Exercice_2.py/exercice.py
@@ -1,7 +1,6 @@
-import sys 
-import re 
+# import sys 
+# import re 
 
-file = open('fichier.txt')
-
-print(file.read())
-
+# Écrivez un programme qui ouvre un fichier texte et qui retrouve tous les nombres qui y apparaissent.
+# Le programme produit en sortie un inventaire de ces nombres, séparés par des virgules et avec le
+# numéro de ligne où ils apparaissent.
